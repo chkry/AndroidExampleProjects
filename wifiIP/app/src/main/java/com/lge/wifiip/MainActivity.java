9f257751a8680978ab4f996f307ddbfe35cd2896
CHAKRADHAR <chakradhar.reddy@lge.com>
2017-09-12 11:22:41 +0530

    [Android] WifiIp android project added
    *displays ip address of device

---
+++ wifiIP/app/src/main/java/com/lge/wifiip/MainActivity.java
@@ -0,0 +1,57 @@
+package com.lge.wifiip;
+
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.format.Formatter;
+import android.widget.TextView;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    //    WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
+      //  String ip = InetAddress.getHostAddress().toString();
+        TextView txtvw = (TextView)findViewById(R.id.txtView);
+        String ipaddr = getIPAddress(true);
+        txtvw.setText(ipaddr);
+
+    }
+
+
+    public static String getIPAddress(boolean useIPv4) {
+        try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for (InetAddress addr : addrs) {
+                    if (!addr.isLoopbackAddress()) {
+                        String sAddr = addr.getHostAddress();
+                        //boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
+                        boolean isIPv4 = sAddr.indexOf(':')<0;
+
+                        if (useIPv4) {
+                            if (isIPv4)
+                                return sAddr;
+                        } else {
+                            if (!isIPv4) {
+                                int delim = sAddr.indexOf('%'); // drop ip6 zone suffix
+                                return delim<0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception ex) { } // for now eat exceptions
+        return "";
+    }
+}
